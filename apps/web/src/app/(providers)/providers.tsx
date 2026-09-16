b6be348967f907528502f9201f333d08bbdd7197
Dima Parinov <addmonside@gmail.com>
2026-09-16 11:57:15 +0300

    ~providers: minor fix

--- apps/web/src/app/(providers)/providers.tsx
+++ apps/web/src/app/(providers)/providers.tsx
@@ -1,11 +1,6 @@
 import { PropsWithChildren } from 'react';
-import { JotaiProvider } from './jotai-provider';
 import { QueryProvider } from './query-provider';
 
 export function Providers({ children }: Readonly<PropsWithChildren<unknown>>) {
-  return (
-    <JotaiProvider>
-      <QueryProvider>{children}</QueryProvider>
-    </JotaiProvider>
-  );
+  return <QueryProvider>{children}</QueryProvider>;
 }
