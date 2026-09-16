2dfe706291e53cced2757392d2f41bddd0da95d5
Dima Parinov <addmonside@gmail.com>
2026-09-16 16:01:32 +0300

    ~ prettier fix

--- apps/web/src/shared/assets/fonts/fonts.ts
+++ apps/web/src/shared/assets/fonts/fonts.ts
@@ -1,21 +1,21 @@
-import { Geist, Geist_Mono, Nunito_Sans } from 'next/font/google'
-import { cn } from '@/shared/lib/cn'
+import { Geist, Geist_Mono, Nunito_Sans } from 'next/font/google';
+import { cn } from '@/shared/lib/cn';
 
-const nunitoSansHeading = Nunito_Sans({ subsets: ['latin'], variable: '--font-heading' })
+const nunitoSansHeading = Nunito_Sans({ subsets: ['latin'], variable: '--font-heading' });
 
 const geistSans = Geist({
   variable: '--font-sans',
   display: 'swap',
   subsets: ['latin'],
-})
+});
 
 const geistMono = Geist_Mono({
   variable: '--font-mono',
   display: 'swap',
   subsets: ['latin'],
-})
+});
 
 export const font = {
   variable: cn(nunitoSansHeading.variable, geistSans.variable, geistMono.variable, 'antialiased'),
   className: cn(geistSans.className),
-}
+};
